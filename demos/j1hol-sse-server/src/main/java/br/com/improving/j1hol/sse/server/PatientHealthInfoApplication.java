025253d05408d3edb1a18857bb8554ee259445c5
David R. Heffelfinger <dheffelfinger@ensode.com>
2018-10-08 18:25:05 -0400

    updated JAX-RS SSE demo code

--- demos/j1hol-sse-server/src/main/java/br/com/improving/j1hol/sse/server/PatientHealthInfoApplication.java
+++
@@ -1,8 +0,0 @@
-package br.com.improving.j1hol.sse.server;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class PatientHealthInfoApplication extends Application {
-}
